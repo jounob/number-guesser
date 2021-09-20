32cf752d296e32a58c5383bdac82eb951f8aee4d
jounob <obeiwiljephnie@gmail.com>
2021-09-20 13:03:57 -0300

    Compare Guesses

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () =>{
+    let random = Math.floor(Math.random() * 10);
+    return random;
+}
+const CompareGuesses = (humanNum, machinNum, secretNum) => {
+    secretNum = generateTarget;
+    machinNum = Math.floor(Math.random() * 10);
+    distansceComputer = Math.abs(machinNum, secretNum);
+    distanceHuman = Math.abs(humanNum, secretNum);
+    if(distanceHuman <= distansceComputer){
+        return true;
+    }
+    else {
+        return false;
+    }
+};
